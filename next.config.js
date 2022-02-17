337e1c127031a5cf1f2f7b725642c15355d569ef
Deepak Singh <theartistnw@gmail.com>
2022-02-17 10:27:40 +0530

    completed bitsom cms

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    /**/
+    PATH_URL : "http://192.168.1.217:1003/api/"
+
+    /*/
+    PATH_URL: "http://localhost:8080",
+    /**/
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import '../styles/style.css'
+import '../styles/main.css'
+import Head from 'next/head'
+import Header from './common/header'
+import { useRouter } from "next/router";
+import "../node_modules/react-datepicker/dist/react-datepicker.css";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+
+  if (router.pathname != "/login" && router.pathname !="/common/protected") {
+    return (
+      <>
+        <Head>
+          <link rel='shortcut icon' href='/image/celect-logo.png' />
+        </Head>
+
+        <Header />
+
+        <Component {...pageProps} />
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <Head>
+          <link rel='shortcut icon' href='/image/celect-logo.png' />
+        </Head>
+
+
+        <Component {...pageProps} />
+      </>
+    )
+  }
+
+
+
+
 }
 
 export default MyApp
+
+
+
